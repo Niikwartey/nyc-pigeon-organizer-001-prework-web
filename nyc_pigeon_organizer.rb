0abf2b9a3d544c7b283604966f918bad40d27eae
Niikwartey <niikwartey1@gmail.com>
2016-07-08 12:27:47 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  names = get_names(data)
+  pigeon_list = {}
+
+  names.each do |name|
+    pigeon_list[name] = {}
+    data.keys.each do |key|
+      pigeon_list[name][key] = []
+    end
+  end
+  
+  pigeon_list.keys.each do |name|
+    data.each do |category, values_hash|
+      values_hash.each do |key, values_arr|
+        if values_arr.include?(name)
+          pigeon_list[name][category] << key.to_s
+        end
+      end
+    end
+  end
+  
+  pigeon_list
+end
+
+#helper methods
+def get_names(data)
+  data.values.map do |attr|
+    attr.values
+  end.flatten.uniq
 end
